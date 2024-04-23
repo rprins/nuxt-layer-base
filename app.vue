ac1d483dedd679a382738f8012cf28c49e403548
Ricardo Prins <ricardo.prins@paqt.com>
2024-04-23 15:19:26 +0200

    Create BaseCard component

--- app.vue
+++ app.vue
@@ -1,3 +1,3 @@
 <template>
-  <HelloWorld />
+  <BaseCard />
 </template>
---
+++ components/BaseCard.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'BaseCard',
+});
+</script>
+
+<template>
+  <template>
+    <UCard>
+      <template #header>
+        Card header
+      </template>
+
+      Card body
+
+      <template #footer>
+        Card footer
+      </template>
+    </UCard>
+  </template>
+</template>
